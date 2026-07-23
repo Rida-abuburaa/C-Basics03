b16c841f25d1308d571f15965b2af421df421d8e
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-07-23 15:17:09 -0700

    Question5

--- Assiment2/Program.cs
+++ Assiment2/Program.cs
@@ -28,10 +28,10 @@ namespace Assiment2
 
             #region Question3
             // Create a StringBuilder, Append() the text "Book List", then Append() " - Updated" onto the same object.Print the final result.
-             /*StringBuilder s1 = new StringBuilder();
-             s1.Append("Book List");
-             s1.Append(" - Updated");
-             Console.WriteLine(s1);*/
+            /*StringBuilder s1 = new StringBuilder();
+            s1.Append("Book List");
+            s1.Append(" - Updated");
+            Console.WriteLine(s1);*/
 
 
 
@@ -40,9 +40,19 @@ namespace Assiment2
 
             #region Question4
             //Using the StringBuilder from the question above, use Replace() to change "Book List" into "Library".  Print the result.
-           /* s1.Replace("Book List" , "Library");
-            Console.WriteLine(s1);*/
-            #endregion 
+            /* s1.Replace("Book List" , "Library");
+             Console.WriteLine(s1);*/
+            #endregion
+
+            #region  Question5
+            //Given string title = "Clean Code"; and int pages = 464;, build the sentence "Book: Clean Code,  Pages: 464" using the + operator. 
+           /* string title = "Clean Code ";
+            int pages = 464;
+            Console.WriteLine("Book:"+ title +"," + "Pages:"+ pages); */
+
+            #endregion
+
+
 
         }
     }
