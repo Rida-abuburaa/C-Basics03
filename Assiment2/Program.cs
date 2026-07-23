69e36a1683ccc2d613368a6cc71ae14c585cff4e
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-07-23 15:44:06 -0700

    Question10

--- Assiment2/Program.cs
+++ Assiment2/Program.cs
@@ -80,7 +80,7 @@ namespace Assiment2
 
             #region Qustion9
             //Given int pages = 464; and bool isAvailable = true;, print "You can borrow this book" only if pages is greater than 300 and isAvailable is true.Use the && operator. 
-            int pages = 464;
+            /*int pages = 464;
             bool isAvailable = true;
             if (pages>300 && isAvailable == true)
             {
@@ -89,12 +89,24 @@ namespace Assiment2
             }else
             {
                 Console.WriteLine("not found");
-            }
+            }*/
 
 
 
             #endregion
+            #region Question10
+            //Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else. 
+           /* string title = "Refactoring";
+            switch (title)
+            {
+                case "Clean Code": Console.WriteLine("Great choice!"); break;
+                case "Refactoring": Console.WriteLine("Nice pick!"); break;
+                default: Console.WriteLine("Never heard of it"); break;
 
+            }*/
+
+
+            #endregion
 
 
 
