a2701a1af290b1fbd97bc772a40b6ca3fe72951d
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-07-23 14:09:00 -0700

    Question1

--- Assiment2/Program.cs
+++ Assiment2/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Assiment2
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace Assiment2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Question1
+            //Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print  both title and upperTitle to show that title did not change.
+            /*string title = "clean code";
+            string upperTitle= title.ToUpper();
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle);*/
+
+            #endregion 
         }
     }
 }
